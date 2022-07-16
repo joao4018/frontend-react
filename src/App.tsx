9397ccfc61c04989f60ff2f7cab5a9c0e6c520e1
joao4018 <joao4018@gmail.com>
2022-07-16 19:22:44 -0300

    fix ci

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
                         <Profile/>
                     </ProtectedLayout>}>
                     </Route>
-                    <Route path='/login' element={<Login/>}>
+                    <Route path='/' element={<Login/>}>
                     </Route>
                     <Route path='/forgot' element={<Forgot/>}>
                     </Route>
